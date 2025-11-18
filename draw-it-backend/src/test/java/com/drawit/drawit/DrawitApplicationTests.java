aa0dac561ac97c81a838cc014c4372cd253e306b
Tai Le <letuantai241@gmail.com>
2025-11-18 14:54:07 +1030

    first commit for backend

---
+++ draw-it-backend/src/test/java/com/drawit/drawit/DrawitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drawit.drawit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
